92dd48eaeba70a248fe7e7febd916957fa511c2b
wangpei <wangpei009@ke.com>
2018-10-09 16:59:20 +0800

    代码整理

--- js/app.js
+++ js/app.js
@@ -3,21 +3,24 @@ var io = require('socket.io')(app);
 var PORT = 8081;
 /*定义用户数组*/
 var users = [];
-var groups=[];
+var groups=[]; //群聊数组 [{username:"群聊"+index,users:data}]  username 群聊名称，users 此群聊中的所有用户名组成的数组
 var groupsObj={};//群聊对象，{key:value}  key:群聊名 value:(数组) 此群聊中的所有用户名组成的数组
 var usocket = {}; //保存每个用户对应的socket {key:value} key:用户名  value：socket
-var catchMessage = [];  //某一个未打开单聊页面的人的消息
-var catchMsgObj = {};  //所有没有打开单聊页面的人的消息
+// var catchMessage = [];  //某一个未打开单聊页面的人的消息
+// var catchMsgObj = {};  //所有没有打开单聊页面的人的消息
 var index=1; //群聊名称
 
 app.listen(PORT);
 
 io.on('connection', function (socket) {
+    /**
+     * 用户登录
+     */
     /*是否是新用户标识*/
     var isNewPerson = true;
     /*当前登录用户*/
     var username = null;
-    var username2 = null; //当前进入单聊页面的用户
+    // var username2 = null; //当前进入单聊页面的用户
     /*监听登录*/
     socket.on('login', function (data) {
         for (var i = 0; i < users.length; i++) {
@@ -37,7 +40,7 @@ io.on('connection', function (socket) {
             usocket[username] = socket;
             /*登录成功*/
             socket.emit('loginSuccess', data);
-            /*向所有连接的客户端广播add事件*/
+            /*向除自己以外的所有连接的客户端广播add事件*/
             socket.broadcast.emit('add', data);
 
             let myGroup=[];
@@ -55,22 +58,9 @@ io.on('connection', function (socket) {
         }
     })
 
-
-    /*退出登录*/
-    socket.on('disconnect', function () {
-        /*向所有连接的客户端广播leave事件*/
-        io.sockets.emit('leave', username);
-
-        delete usocket[username];  //删除离开的用户的usocket
-        catchMsgObj[username2] && delete catchMsgObj[username2];
-
-        users.map(function (val, index) {
-            if (val.username === username) {
-                users.splice(index, 1);
-            }
-        })
-    })
-
+    /**
+     * 发送消息
+     */
     //私聊
     socket.on("send private message", function (res) {
         console.log(res);
@@ -94,6 +84,9 @@ io.on('connection', function (socket) {
         }
     })
 
+    /**
+     * 创建群聊
+     */
     //创建群聊页面显示所有在线用户
     socket.on("get users",function () {
         console.log("get users");
@@ -109,6 +102,25 @@ io.on('connection', function (socket) {
         }
         index++;
     })
+
+    /**
+     * 用户断开连接
+     */
+    /*退出登录*/
+    socket.on('disconnect', function () {
+        /*向所有连接的客户端广播leave事件*/
+        io.sockets.emit('leave', username);
+
+        delete usocket[username];  //删除离开的用户的usocket
+        // catchMsgObj[username2] && delete catchMsgObj[username2];  //缓存消息
+
+        users.map(function (val, index) {
+            if (val.username === username) {
+                users.splice(index, 1);
+            }
+        })
+    })
+
 })
 
 console.log('app listen at' + PORT);
--- js/chat.js
+++ js/chat.js
@@ -1,9 +1,12 @@
 $(function(){
 	/*建立socket连接，使用websocket协议，端口号是服务器端监听端口号*/
-	var socket = io('ws://10.33.134.128:8081');
+	var socket = io('ws://10.33.134.77:8081');
 	/*定义用户名*/
 	var uname = null;
 
+    /**
+     * 用户登录
+     */
 	/*登录*/
 	$('.login-btn').click(function(){
 		uname = $.trim($('#loginName').val());
@@ -42,73 +45,43 @@ $(function(){
 	socket.on('users',function(users){
         let head="../images/user/user.jpg";
 		for(var i=0;i<users.length;i++){
+		    if(users[i].username.indexOf("群聊")!==-1){
+                head="../images/user/group.png";
+            }else {
+                head="../images/user/user.jpg";
+            }
             showUsers(users[i].username,head);
             messageBox(users[i].username);
 		}
 	})
 
-	/*退出群聊提示*/
-	socket.on('leave',function(name){
-		if(name != null){
-			deleteDom(name);
-		}
-	})
-
-	//某一个用户退出时，在用户列表删除改用户并且删除改用户对应的聊天框
-	function deleteDom(username) {
-        $('#session')[0].removeChild($('#li'+hex_md5(username))[0]);
-        $('#chat')[0].removeChild($('#'+hex_md5(username))[0]);
+    /*隐藏登录界面 显示聊天界面*/
+    function checkin(data){
+        $('.login-wrap').hide('slow');
+        $('.app').show('slow');
     }
 
-	/*隐藏登录界面 显示聊天界面*/
-	function checkin(data){
-		$('.login-wrap').hide('slow');
-		$('.app').show('slow');
-	}
-
-	/*显示消息*/
-	function showMessage(mark,data){
-		var html;
-		if(mark==="group"){
-            if(data.addresser === uname){
-                html = '<div class="chat-item item-right clearfix"><span class="img fr"></span><span class="message fr">'+data.body+'</span></div>'
-            }
-            else {
-                html='<div class="chat-item item-left clearfix rela"><span class="abs uname">'+data.addresser+'</span><span class="img fl"></span><span class="fl message">'+data.body+'</span></div>'
-            }
-            $('#'+hex_md5(data.recipient)+' .chat-msg').append(html);
-		}
-		else {
-            if(data.addresser === uname){
-                html = '<div class="chat-item item-right clearfix"><span class="img fr"></span><span class="message fr">'+data.body+'</span></div>'
-                $('#'+hex_md5(data.recipient)+' .chat-msg').append(html);
-            }
-            if(data.recipient === uname){
-                html='<div class="chat-item item-left clearfix rela"><span class="abs uname">'+data.addresser+'</span><span class="img fl"></span><span class="fl message">'+data.body+'</span></div>'
-                $('#'+hex_md5(data.addresser)+' .chat-msg').append(html);
-            }
-		}
-
-	}
-
-	/*显示用户*/
-	function  showUsers(tname,head) {
+    /*显示用户*/
+    function  showUsers(tname,head) {
         $('#session').append('<li id="li' + hex_md5(tname) + '"><img src="' + head + '"><span class="nick-name">' + tname + '</span></li>');
     }
 
     /*消息框*/
-	function messageBox(tname) {
+    function messageBox(tname) {
         var html = '';
         html += '<div id="' + hex_md5(tname) + '" data-n="' + tname + '" class="chat"><div class="main">';
         html += '<div class="message"><div class="head"><p>' + tname + '</p></div>';
         html += '<div class="body"><ul class="chat-msg"></ul></div></div>';
         html += '<div class="footer"><div class="box"><div class="head">';
-        html += '<svg class="icon emoji" style="" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4692" xmlns:xlink="http://www.w3.org/1999/xlink"><defs></defs><path d="M520.76544 767.05792c-99.14368 0-180.30592-73.65632-193.78176-169.09312l-49.22368 0c13.78304 122.624 116.61312 218.29632 242.91328 218.29632S749.81376 720.5888 763.5968 597.9648l-49.0496 0C701.0816 693.4016 619.90912 767.05792 520.76544 767.05792zM512 0C229.23264 0 0 229.2224 0 512c0 282.75712 229.23264 512 512 512 282.76736 0 512-229.24288 512-512C1024 229.2224 794.76736 0 512 0zM511.95904 972.78976C257.46432 972.78976 51.1488 766.48448 51.1488 512c0-254.49472 206.30528-460.81024 460.81024-460.81024 254.48448 0 460.8 206.30528 460.8 460.81024C972.75904 766.48448 766.44352 972.78976 511.95904 972.78976zM655.57504 456.92928c31.06816 0 56.24832-25.1904 56.24832-56.24832 0-31.06816-25.18016-56.24832-56.24832-56.24832-31.06816 0-56.25856 25.18016-56.25856 56.24832C599.31648 431.73888 624.49664 456.92928 655.57504 456.92928zM362.73152 456.92928c31.06816 0 56.24832-25.1904 56.24832-56.24832 0-31.06816-25.1904-56.24832-56.24832-56.24832-31.0784 0-56.25856 25.18016-56.25856 56.24832C306.47296 431.73888 331.65312 456.92928 362.73152 456.92928z" p-id="4693"></path></svg>';
+        html += '<svg class="icon emoji" style="display: none" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4692" xmlns:xlink="http://www.w3.org/1999/xlink"><defs></defs><path d="M520.76544 767.05792c-99.14368 0-180.30592-73.65632-193.78176-169.09312l-49.22368 0c13.78304 122.624 116.61312 218.29632 242.91328 218.29632S749.81376 720.5888 763.5968 597.9648l-49.0496 0C701.0816 693.4016 619.90912 767.05792 520.76544 767.05792zM512 0C229.23264 0 0 229.2224 0 512c0 282.75712 229.23264 512 512 512 282.76736 0 512-229.24288 512-512C1024 229.2224 794.76736 0 512 0zM511.95904 972.78976C257.46432 972.78976 51.1488 766.48448 51.1488 512c0-254.49472 206.30528-460.81024 460.81024-460.81024 254.48448 0 460.8 206.30528 460.8 460.81024C972.75904 766.48448 766.44352 972.78976 511.95904 972.78976zM655.57504 456.92928c31.06816 0 56.24832-25.1904 56.24832-56.24832 0-31.06816-25.18016-56.24832-56.24832-56.24832-31.06816 0-56.25856 25.18016-56.25856 56.24832C599.31648 431.73888 624.49664 456.92928 655.57504 456.92928zM362.73152 456.92928c31.06816 0 56.24832-25.1904 56.24832-56.24832 0-31.06816-25.1904-56.24832-56.24832-56.24832-31.0784 0-56.25856 25.18016-56.25856 56.24832C306.47296 431.73888 331.65312 456.92928 362.73152 456.92928z" p-id="4693"></path></svg>';
         html += '</div><div class="body"><input type="text" class="input" /></div>';
         html += '<div class="foot"><a class="send" href="javascript:void(0)">发送(Enter)</a></div></div></div></div></div>';
         $('#chat').append(html);
     }
 
+    /**
+     * 发送消息
+     */
     //active li
     $(document).on('click', '#session li', function () {
         $('.active').removeClass('active');
@@ -132,7 +105,7 @@ $(function(){
 
     //sendMessage  发送消息
     function toSendMessage(){
-    	debugger
+        debugger
         var recipient = $('.chat-active').attr('data-n');
         var val = $('.chat-active input').val();
         if(val == '') return;
@@ -144,11 +117,11 @@ $(function(){
             'body':val
         }
         if(recipient.indexOf("群聊")!==-1){
-        	req.messType="group";
-        	socket.emit("send group message",req);
-		}else {
+            req.messType="group";
+            socket.emit("send group message",req);
+        }else {
             socket.emit('send private message', req);
-		}
+        }
 
         $('.chat-active input').val('');
     }
@@ -156,7 +129,7 @@ $(function(){
     // 接收私聊信息
     socket.on('receive private message', function (data) {
         console.log(data);
-		showMessage("private",data)
+        showMessage("private",data)
 
     });
 
@@ -165,26 +138,54 @@ $(function(){
         showMessage("group",data)
     })
 
+    /*显示消息*/
+    function showMessage(mark,data){
+        var html;
+        if(mark==="group"){
+            if(data.addresser === uname){
+                html = '<div class="chat-item item-right clearfix"><span class="img fr"></span><span class="message fr">'+data.body+'</span></div>'
+            }
+            else {
+                html='<div class="chat-item item-left clearfix rela"><span class="abs uname">'+data.addresser+'</span><span class="img fl"></span><span class="fl message">'+data.body+'</span></div>'
+            }
+            $('#'+hex_md5(data.recipient)+' .chat-msg').append(html);
+        }
+        else {
+            if(data.addresser === uname){
+                html = '<div class="chat-item item-right clearfix"><span class="img fr"></span><span class="message fr">'+data.body+'</span></div>'
+                $('#'+hex_md5(data.recipient)+' .chat-msg').append(html);
+            }
+            if(data.recipient === uname){
+                html='<div class="chat-item item-left clearfix rela"><span class="abs uname">'+data.addresser+'</span><span class="img fl"></span><span class="fl message">'+data.body+'</span></div>'
+                $('#'+hex_md5(data.addresser)+' .chat-msg').append(html);
+            }
+        }
+
+    }
+
+    /**
+     * 创建群聊
+     */
 
     //点击➕号添加群聊
-	$(".add-group").click(function () {
-		$("#user-list").html("");
-		socket.emit("get users");
+    $(".add-group").click(function () {
+        $("#user-list").html("");
+        socket.emit("get users");
         $('.app').hide('slow');
         $('.user-list-box').show('slow');
     })
 
-	//创建群聊页面显示所有在线用户不显示自己
-	socket.on("receive users",function (users) {
-		for(var i=0;i<users.length;i++){
-			if(users[i].username!==uname){
-				var touxiang="../images/user/user.jpg";
-				showUserList(users[i].username,touxiang);
-			}
-		}
+    //创建群聊页面显示所有在线用户不显示自己
+    socket.on("receive users",function (users) {
+        for(var i=0;i<users.length;i++){
+            if(users[i].username!==uname){
+                var touxiang="../images/user/user.jpg";
+                showUserList(users[i].username,touxiang);
+            }
+        }
     })
 
-	function showUserList(name,head) {
+    function showUserList(name,head) {
         $('#user-list').append('<li id="g-' + name + '"><img src="' + head + '"><span class="nick-name">' + name + '</span><input id="'+name+'" type="checkbox"/></li>');
     }
 
@@ -193,20 +194,37 @@ $(function(){
         $('.user-list-box').hide('slow');
     })
 
-	$("#confirm-btn").click(function () {
-		var checkedUsers=[];
-		if($("input:checked").length<2){
-			alert("创建群聊至少选择两个人");
-			return;
-		}
+    $("#confirm-btn").click(function () {
+        var checkedUsers=[];
+        if($("input:checked").length<2){
+            alert("创建群聊至少选择两个人");
+            return;
+        }
 
-		$("input:checked").each(function () {
-			checkedUsers.push($(this)[0].id);
+        $("input:checked").each(function () {
+            checkedUsers.push($(this)[0].id);
         });
-		checkedUsers.push(uname)
-		socket.emit("add group",checkedUsers);
+        checkedUsers.push(uname)
+        socket.emit("add group",checkedUsers);
         $('.app').show('slow');
         $('.user-list-box').hide('slow');
     })
+
+
+    /**
+     * 用户断开连接
+     */
+	/*退出聊天*/
+	socket.on('leave',function(name){
+		if(name != null){
+			deleteDom(name);
+		}
+	})
+
+	//某一个用户退出时，在用户列表删除改用户并且删除改用户对应的聊天框
+	function deleteDom(username) {
+        $('#session')[0].removeChild($('#li'+hex_md5(username))[0]);
+        $('#chat')[0].removeChild($('#'+hex_md5(username))[0]);
+    }
 
 })
